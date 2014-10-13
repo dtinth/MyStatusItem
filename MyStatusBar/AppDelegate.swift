7b612d3115f5016553a65592d6d72757b1a13233
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-10-13 10:44:26 +0700

    Initial Commit

---
+++ MyStatusBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MyStatusBar
+//
+//  Created by Thai Pangsakulyanont on 2014/10/13.
+//  Copyright (c) 2014年 Thai Pangsakulyanont. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
